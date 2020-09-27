ce80f8dc1b85b6b14aecbbb2dd3336f3ebe9df23
pketong <13680586193@139.com>
2020-09-27 22:48:21 +0800

    控件交互学习

--- learn_python/test_selenium/test_selenium.py
+++ learn_python/test_selenium/test_selenium.py
@@ -6,7 +6,8 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
-#定义类
+
+# 定义类
 class TestHome:
 
     def setup(self):
@@ -14,13 +15,13 @@ class TestHome:
         self.driver.maximize_window()
         self.driver.implicitly_wait(5)
         # self.driver.get("https://www.baidu.com")
-        self.driver.get('http://sahitest.com/demo/clicks.htm')
-
 
     def teardown(self):
         self.driver.quit()
 
+    @pytest.mark.skip
     def test_case_click(self):
+        self.driver.get('http://sahitest.com/demo/clicks.htm')
         # self.driver.find_element(By.CSS_SELECTOR,'#kw').send_keys("霍格沃兹测试学院")
         # sleep(2)
         elemenet_click = self.driver.find_element_by_xpath('//*[@value="click me"]')
@@ -33,6 +34,23 @@ class TestHome:
         action.perform()
         sleep(3)
 
+    def test_movetoelement(self):
+        self.driver.get("http://www.baidu.com")
+        # ele = self.driver.find_element_by_xpath('//*[@name="tj_briicon"]')
+        ele = self.driver.find_element_by_xpath('//*[@id="s-usersetting-top"]')
 
-if __name__ == '__main__':
-    pytest.main(['-v', '-s', 'test_selenium.py'])
+        action = ActionChains(self.driver)
+        action.move_to_element(ele)
+        action.perform()
+        sleep(5)
+
+    @pytest.mark.skip
+    def test_dragdrop(self):
+        self.driver.get("http://sahitest.com/demo/dragDropMooTools.htm")
+        drag_element = self.driver.find_element_by_id("dragger")
+        drop_element = self.driver.find_element_by_xpath("/html/body/div[2]")
+        action = ActionChains(self.driver)
+        action.drag_and_drop(drag_element, drop_element).perform()
+        sleep(5)
+# if __name__ == '__main__':
+#  pytest.main(['-v', '-s', 'test_selenium.py'])
